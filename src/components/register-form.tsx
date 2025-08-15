75ba3bf22c271b563f09ca59c0f02d164a569f90
Adrian Bengolea <abengolea1@gmail.com>
2025-08-15 11:49:21 +0000

    cuando pongo parap oner la edad deberia moverse rapidamente para ir a de

--- src/components/register-form.tsx
+++ src/components/register-form.tsx
@@ -159,6 +159,9 @@ export function RegisterForm() {
                                     <PopoverContent className="w-auto p-0" align="start">
                                     <Calendar
                                         mode="single"
+                                        captionLayout="dropdown-buttons"
+                                        fromYear={1950}
+                                        toYear={new Date().getFullYear()}
                                         selected={field.value}
                                         onSelect={field.onChange}
                                         disabled={(date) =>
